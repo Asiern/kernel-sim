db6f219c2970e2dfc8228bd97fddbc281b701c42
Asiern <asnulorri@gmail.com>
2021-10-22 11:03:14 +0200

    fixed: statement with no effect warning

--- src/machine.c
+++ src/machine.c
@@ -6,6 +6,6 @@ void init_machine(machine* m, int n_cpus, int n_cores, int n_threads)
 {
     int i = 0;
     m->cpus = (cpu*)malloc(n_cpus * sizeof(cpu));
-    for (i; i < n_cpus; i++)
+    for (; i < n_cpus; i++)
         init_cpu(&(m->cpus[i]), n_cores, n_threads);
 }
