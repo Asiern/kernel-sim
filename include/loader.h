e74300af44e3e130c0020615688adf22d8b1f326
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2022-01-15 12:01:11 +0100

    fix: validate

--- include/loader.h
+++ include/loader.h
@@ -1,3 +1,8 @@
 #include "types.h"
 
-pcb load_pcb(void);
+#define extension ".elf"
+
+int last_program_id;
+
+void* loader(void);
+int validate(const char* filename);
--- src/loader.c
+++ src/loader.c
@@ -1,10 +1,45 @@
 #include "loader.h"
+#include "dirent.h"
+#include "utils.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-pcb load_pcb(void)
+void* loader(void)
 {
-    // Open file
-    FILE* f = fopen("fitx.txt", "r");
+    DIR* dir;
+    struct dirent* ent;
 
-    // TODO Read file
+    if ((dir = opendir("./input")) != NULL)
+    {
+        while ((ent = readdir(dir)) != NULL)
+        {
+            // TODO Validate entry
+            if (validate(ent->d_name) == 1)
+                continue;
+            printf("Found valid program %s\n", ent->d_name);
+
+            // TODO Compare entry ID with latest program id
+            if (0 == 0)
+            {
+                // Open program
+                FILE* f = fopen(ent->d_name, "r");
+            }
+        }
+    }
+    else
+    {
+        cprint("Error input folder not found\n", RED);
+        exit(-1);
+    }
+}
+
+int validate(const char* filename)
+{
+    char* ldot = strrchr(filename, '.');
+    if (strlen(ldot) == 1)
+        return 1;
+    if (ldot != NULL)
+        return strcmp(ldot + 1, ".elf");
+    return 1;
 }
